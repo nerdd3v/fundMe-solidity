5e7f314768c77eceab9188e830b23aad50be2382
n3rddev <n3rddev.code@gmail.com>
2026-02-19 21:41:14 +0530

    test owner withdrawal success

--- FundMe/test/FundMe.t.sol
+++ FundMe/test/FundMe.t.sol
@@ -20,11 +20,16 @@ contract CounterTest is Test {
         assertEq(deployedContract.getOwner(), 0x20d99Eb01562f040e1c2716bd7cB48C5b450b2D0, "the owner does not match");
     }
 
-    function testOwnerOnlyWithdrawal()public{
+    function testOwnerOnlyWithdrawal()public payable{
         address alice = makeAddr("alice");
+        vm.deal(alice, 10 ether);
         vm.prank(0x20d99Eb01562f040e1c2716bd7cB48C5b450b2D0);
         deployedContract = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        vm.prank(0x20d99Eb01562f040e1c2716bd7cB48C5b450b2D0);
+        //fund
+        vm.prank(alice);
+        deployedContract.fundMe{value: 2 ether}("alice");
+        vm.prank(alice);
+        vm.expectRevert();
         deployedContract.withdrawal();
     }
 
